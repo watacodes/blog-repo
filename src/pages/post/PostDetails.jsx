2d62af3427a09627e8679eceef601c89ac072ac7
watacodes <watafromjapan@gmail.com>
2025-01-14 15:27:18 +0900

    課題3　実装

--- src/pages/post/PostDetails.jsx
+++ src/pages/post/PostDetails.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
-import { posts } from "../../data/posts";
 import dayjs from "dayjs";
 import CategoryButton from "../../components/CategoryButton";
 import NotFound from "../notfound/NotFound";
+import { useContext } from "react";
+import { PostContext } from "../../context/PostContext";
+import Loading from "./components/Loading";
 
 const PostDetails = () => {
   const { id } = useParams();
+  const { posts, isLoading } = useContext(PostContext);
+
   const targetPost = posts.find((post) => post.id.toString() === id);
 
   if (!targetPost) return <NotFound />;
@@ -13,31 +17,37 @@ const PostDetails = () => {
   const date = dayjs(targetPost.createdAt).format("MM/DD/YYYY");
 
   return (
-    <div className="flex flex-col justify-center items-center p-2">
-      <div>
-        <div className="mt-10 mb-5">
-          <img
-            className="h-auto max-w-full"
-            src={`${targetPost.thumbnailUrl}`}
-            alt="A thumbnail of the post"
-          />
-        </div>
-        <div className="p-3">
-          <div className="flex justify-between">
-            <div className="text-sm text-gray-400">{date}</div>
-            <div className="flex px-4">
-              {targetPost.categories.map((category, index) => (
-                <CategoryButton category={category} key={index} />
-              ))}
+    <div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="flex flex-col justify-center items-center p-2">
+          <div>
+            <div className="mt-10 mb-5">
+              <img
+                className="h-auto max-w-full"
+                src={`${targetPost.thumbnailUrl}`}
+                alt="A thumbnail of the post"
+              />
+            </div>
+            <div className="p-3">
+              <div className="flex justify-between">
+                <div className="text-sm text-gray-400">{date}</div>
+                <div className="flex px-4">
+                  {targetPost.categories.map((category, index) => (
+                    <CategoryButton category={category} key={index} />
+                  ))}
+                </div>
+              </div>
+              <h1 className="py-5 text-2xl">{targetPost.title}</h1>
+              <div
+                className="overflow-hidden min-w-fit"
+                dangerouslySetInnerHTML={{ __html: targetPost.content }}
+              />
             </div>
           </div>
-          <h1 className="py-5 text-2xl">{targetPost.title}</h1>
-          <div
-            className="overflow-hidden min-w-fit"
-            dangerouslySetInnerHTML={{ __html: targetPost.content }}
-          />
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,7 +1,10 @@
-import { posts } from "../../data/posts";
+import { useContext } from "react";
+import { PostContext } from "../../context/PostContext";
 import Post from "./components/Post";
 
 const Posts = () => {
+  const { posts } = useContext(PostContext);
+
   return (
     <main className="flex flex-col items-center m-10 py-4 min-h-full">
       <ul className="max-w-[800px]">
